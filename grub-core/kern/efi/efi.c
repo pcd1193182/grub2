4f0f0640d3ba56ceaf6833dc7e05d74c3d4c1e1a
Ubuntu Git Importer <usd-importer-do-not-mail@canonical.com>
2018-03-07 04:26:42 +0000

    New upstream version 2.00+20131208

--- grub-core/kern/efi/efi.c
+++ grub-core/kern/efi/efi.c
@@ -181,6 +181,36 @@ grub_efi_set_virtual_address_map (grub_efi_uintn_t memory_map_size,
   return grub_error (GRUB_ERR_IO, "set_virtual_address_map failed");
 }
 
+grub_err_t
+grub_efi_set_variable(const char *var, const grub_efi_guid_t *guid,
+		      void *data, grub_size_t datasize)
+{
+  grub_efi_status_t status;
+  grub_efi_runtime_services_t *r;
+  grub_efi_char16_t *var16;
+  grub_size_t len, len16;
+
+  len = grub_strlen (var);
+  len16 = len * GRUB_MAX_UTF16_PER_UTF8;
+  var16 = grub_malloc ((len16 + 1) * sizeof (var16[0]));
+  if (!var16)
+    return grub_errno;
+  len16 = grub_utf8_to_utf16 (var16, len16, (grub_uint8_t *) var, len, NULL);
+  var16[len16] = 0;
+
+  r = grub_efi_system_table->runtime_services;
+
+  status = efi_call_5 (r->set_variable, var16, guid, 
+		       (GRUB_EFI_VARIABLE_NON_VOLATILE
+			| GRUB_EFI_VARIABLE_BOOTSERVICE_ACCESS
+			| GRUB_EFI_VARIABLE_RUNTIME_ACCESS),
+		       datasize, data);
+  if (status == GRUB_EFI_SUCCESS)
+    return GRUB_ERR_NONE;
+
+  return grub_error (GRUB_ERR_IO, "could not set EFI variable `%s'", var);
+}
+
 void *
 grub_efi_get_variable (const char *var, const grub_efi_guid_t *guid,
 		       grub_size_t *datasize_out)
@@ -277,9 +307,41 @@ grub_efi_modules_addr (void)
 #pragma GCC diagnostic error "-Wcast-align"
 
 char *
-grub_efi_get_filename (grub_efi_device_path_t *dp)
+grub_efi_get_filename (grub_efi_device_path_t *dp0)
 {
-  char *name = 0;
+  char *name = 0, *p;
+  grub_size_t filesize = 0;
+  grub_efi_device_path_t *dp;
+
+  dp = dp0;
+
+  while (1)
+    {
+      grub_efi_uint8_t type = GRUB_EFI_DEVICE_PATH_TYPE (dp);
+      grub_efi_uint8_t subtype = GRUB_EFI_DEVICE_PATH_SUBTYPE (dp);
+
+      if (type == GRUB_EFI_END_DEVICE_PATH_TYPE)
+	break;
+      if (type == GRUB_EFI_MEDIA_DEVICE_PATH_TYPE
+	       && subtype == GRUB_EFI_FILE_PATH_DEVICE_PATH_SUBTYPE)
+	{
+	  grub_efi_uint16_t len;
+	  len = ((GRUB_EFI_DEVICE_PATH_LENGTH (dp) - 4)
+		 / sizeof (grub_efi_char16_t));
+	  filesize += GRUB_MAX_UTF8_PER_UTF16 * len + 1;
+	}
+
+      dp = GRUB_EFI_NEXT_DEVICE_PATH (dp);
+    }
+
+  if (!filesize)
+    return NULL;
+
+  dp = dp0;
+
+  p = name = grub_malloc (filesize);
+  if (!name)
+    return NULL;
 
   while (1)
     {
@@ -293,45 +355,25 @@ grub_efi_get_filename (grub_efi_device_path_t *dp)
 	{
 	  grub_efi_file_path_device_path_t *fp;
 	  grub_efi_uint16_t len;
-	  char *p;
-	  grub_size_t size;
 
-	  if (name)
-	    {
-	      size = grub_strlen (name);
-	      name[size] = '/';
-	      size++;
-	    }
-	  else
-	    size = 0;
+	  if (p != name)
+	    *p++ = '/';
 
 	  len = ((GRUB_EFI_DEVICE_PATH_LENGTH (dp) - 4)
 		 / sizeof (grub_efi_char16_t));
-	  p = grub_realloc (name, size + len * 4 + 1);
-	  if (! p)
-	    {
-	      grub_free (name);
-	      return 0;
-	    }
-
-	  name = p;
 	  fp = (grub_efi_file_path_device_path_t *) dp;
-	  *grub_utf16_to_utf8 ((grub_uint8_t *) name + size,
-			       fp->path_name, len) = '\0';
+	  p = (char *) grub_utf16_to_utf8 ((unsigned char *) p, fp->path_name, len);
 	}
 
       dp = GRUB_EFI_NEXT_DEVICE_PATH (dp);
     }
 
-  if (name)
-    {
-      /* EFI breaks paths with backslashes.  */
-      char *p;
+  *p = '\0';
 
-      for (p = name; *p; p++)
-	if (*p == '\\')
-	  *p = '/';
-    }
+  /* EFI breaks paths with backslashes.  */
+  for (p = name; *p; p++)
+    if (*p == '\\')
+      *p = '/';
 
   return name;
 }
@@ -699,12 +741,17 @@ grub_efi_print_device_path (grub_efi_device_path_t *dp)
 	    case GRUB_EFI_FILE_PATH_DEVICE_PATH_SUBTYPE:
 	      {
 		grub_efi_file_path_device_path_t *fp;
-		grub_uint8_t buf[(len - 4) * 2 + 1];
+		grub_uint8_t *buf;
 		fp = (grub_efi_file_path_device_path_t *) dp;
-		*grub_utf16_to_utf8 (buf, fp->path_name,
-				     (len - 4) / sizeof (grub_efi_char16_t))
-		  = '\0';
+		buf = grub_malloc ((len - 4) * 2 + 1);
+		if (buf)
+		  *grub_utf16_to_utf8 (buf, fp->path_name,
+				       (len - 4) / sizeof (grub_efi_char16_t))
+		    = '\0';
+		else
+		  grub_errno = GRUB_ERR_NONE;
 		grub_printf ("/File(%s)", buf);
+		grub_free (buf);
 	      }
 	      break;
 	    case GRUB_EFI_PROTOCOL_DEVICE_PATH_SUBTYPE:
